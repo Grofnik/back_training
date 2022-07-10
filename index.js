e8e13a08076b8f8d1ca97e3ab29503d64d416b31
Grofty <maksimka.mironov123@mail.ru>
2022-07-10 18:50:11 +0300

    Добавление регистрации и авторизации + исправление некоторых косяков

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const ToDo = require('./db/models/ToDo.model');
+const users = require('./db/models/users.model');
 const { initDB } = require('./db');
 const app = express();
 
@@ -16,47 +17,46 @@ app.use((req, res, next) => {
   next();
 });
 
-app.all('/sum', (req, res) => {
-  const firstNum = req.query.firstNum;
-  const secondNum = req.query.secondNum;
-  const vyvod = parseInt(firstNum)+parseInt(secondNum);
-  res.send('<h1>'+vyvod+'</h1>');
+app.post("/todos/registration", function(req,res){
+  const mail = req.body.mail;
+  const password = req.body.password;
+  users.create({
+    mail,
+    password,
+  }).then((result) => {
+    console.log(result);
+    res.json(result);
+  }).catch((error) => { 
+    console.log(error)
+    res.status(500).json({error});
+  });
 })
 
-app.all('/reverseCase', (req,res) =>{
-  let stroka=req.query.stroka;
-  let result='';
-  for(let i = 0;i<stroka.length;i++)
+app.post("/todos/login", function(req, res)
+{
+const results= NULL;
+results = users.findAll(
   {
-    if(stroka[i]==stroka[i].toUpperCase())
-      result+=stroka[i].toLowerCase();
-    else
-      result+=stroka[i].toUpperCase();
+    where:{
+      mail: req.body.mail,
+      password: req.body.password
+    }
   }
-  res.json({message:result});
+)
+if (results)
+  return true;
+else  
+  return false;
 })
 
-app.all('/reverseArray', (req, res) => {
-  let arrays=req.query.array;
-  let result='<ul>';
-  for(let i=arrays.length-1;i>=0;i--)
-    result+='<li>'+arrays[i]+'</li>';
-  result+='</ul>'
-  res.send(result);
-})
-
-http.createServer(app).listen(3001, () => {
-  console.log('Server is working on port 3001');
-})
-//Практика номер 2
-
-
-app.post("/api/todos", function (request, response) {
+app.post("/api/todos", function (req, response) {
   const title = req.body.title;
   const description = req.body.description;
+  const doing = req.body.description;
   ToDo.create({
     title,
     description,
+    doing,
   }).then((result) => {
     console.log(result);
     response.json(result);
@@ -72,7 +72,6 @@ app.get('/todos', async(req, res)=>{
   return res.status(200).json(results);
 });
 
-
 app.get('/todos/:id', async(req, res)=>{
   const task=await ToDo.findAll(
     {
@@ -84,15 +83,7 @@ app.get('/todos/:id', async(req, res)=>{
   return res.status(200).json(task);
 });
 
-app.post('', async(req, res) => {
-
-});
-
 app.post('/todos/import', async(req, res)=>{
-  let bodyExample = {
-    title: "wertt",
-    description: "wertt"
-  };
   if ((typeof req.body.title) !== "string" || (typeof req.body.description) !== "string") {
   return res.status(400).json({ message: 'field title was not sent.'});
   }
@@ -118,6 +109,7 @@ app.delete('/todos', async(req, res)=>{
     truncate: true,
   })
   return res.status(200).json(task);
+  
 });
 
 app.delete('/todos/:id', async(req, res)=>{
@@ -130,7 +122,9 @@ app.delete('/todos/:id', async(req, res)=>{
   return res.status(200).json(task);
 });
 
-
+http.createServer(app).listen(3001, () => {
+  console.log('Server is working on port 3001');
+})
 
 
 
